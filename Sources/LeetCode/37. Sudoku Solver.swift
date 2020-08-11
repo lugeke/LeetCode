f84d8c7c6f192d7270cc25fa97920be478f60264
jiaheng <lugeke@gmail.com>
2020-08-11 13:26:09 +0800

    improve solveSudoku

--- Sources/LeetCode/37. Sudoku Solver.swift
+++ Sources/LeetCode/37. Sudoku Solver.swift
@@ -7,44 +7,75 @@
 
 import Foundation
 
-let positions: [[(Int, Int)]] = {
-    var result = [[(Int, Int)]]()
+func solveSudoku(_ board: inout [[Character]]) {
     
-    // row
-    for i in 0..<9 {
-        var r = [(Int, Int)]()
-        for j in 0..<9 {
-            r.append((i, j))
+    struct EmptyPostions {
+        struct Point: Hashable {
+            let i: Int
+            let j: Int
         }
-        result.append(r)
-    }
-    // col
-    for i in 0..<9 {
-        var r = [(Int, Int)]()
-        for j in 0..<9 {
-            r.append((j, i))
-        }
-        result.append(r)
-    }
-    // sub-box
-    for k in 0..<9 {
-        var r = [(Int, Int)]()
-        for i in (k/3)*3 + 0..<(k/3)*3 + 3 {
-            for j in (k%3)*3 + 0..<(k%3)*3 + 3 {
-                r.append((i, j))
+        var emptyPoints = Set<Point>()
+        var valuesToFill: Set<Character> = ["1" as Character, "2", "3", "4", "5" ,"6", "7", "8", "9"]
+        var count: Int { valuesToFill.count }
+        
+        
+        mutating func process(point: Point, value: Character) {
+            if value != "." {
+                valuesToFill.remove(value)
+            } else {
+                emptyPoints.insert(point)
             }
         }
-        result.append(r)
+        
+        mutating func fill(point: Point, value: Character) {
+            emptyPoints.remove(point)
+            valuesToFill.remove(value)
+            assert(emptyPoints.count == valuesToFill.count)
+        }
+        
+        mutating func empty(point: Point, originalValue: Character) {
+            emptyPoints.insert(point)
+            valuesToFill.insert(originalValue)
+            assert(emptyPoints.count == valuesToFill.count)
+        }
     }
-    return result
-}()
-
-func solveSudoku1(_ board: inout [[Character]]) {
     
-    struct EmptyPostion {
-        var points = [(Int, Int)]()
-        var values = ["1" as Character, "2", "3", "4", "5" ,"6", "7", "8", "9"]
-        var count: Int { values.count }
+    enum Positon {
+        case row(Int)
+        case col(Int)
+        case grid(Int)
+    }
+    
+    func findEmptyPositions(_ p: Positon) -> EmptyPostions {
+        
+        var empty = EmptyPostions()
+        switch p {
+        case .row(let row):
+            for col in 0..<9 {
+                empty.process(point: .init(i: row, j: col), value: board[row][col])
+            }
+        case .col(let col):
+            for row in 0..<9 {
+                empty.process(point: .init(i: row, j: col), value: board[row][col])
+            }
+        case .grid(let grid):
+            for row in grid/3*3..<grid/3*3+3 {
+                for col in grid%3*3..<grid%3*3+3 {
+                    empty.process(point: .init(i: row, j: col), value: board[row][col])
+                }
+            }
+        }
+        return empty
+    }
+    
+    var emptyRows: [EmptyPostions] = []
+    var emptyCols: [EmptyPostions] = []
+    var emptyGrids: [EmptyPostions] = []
+    
+    for i in 0..<9 {
+        emptyRows.append(findEmptyPositions(.row(i)))
+        emptyCols.append(findEmptyPositions(.col(i)))
+        emptyGrids.append(findEmptyPositions(.grid(i)))
     }
     
     @discardableResult func solveSudoku(_ board: inout [[Character]]) -> Bool {
@@ -54,7 +85,6 @@ func solveSudoku1(_ board: inout [[Character]]) {
             for k in 0..<9 {
                 if board[i][k] == val || board[k][j] == val {  return false }
             }
-            
             // grid
             for i in (i/3)*3 + 0..<(i/3)*3 + 3 {
                 for j in (j/3)*3 + 0..<(j/3)*3 + 3 {
@@ -64,52 +94,49 @@ func solveSudoku1(_ board: inout [[Character]]) {
             return true
         }
         
-        func findEmptyPosition<S>(_ positions: S) -> EmptyPostion where S: Sequence, S.Element == (Int, Int){
-            var empty = EmptyPostion()
-            for p in positions {
-                if board[p.0][p.1] != "." {
-                    empty.values.remove(at: empty.values.firstIndex(of: board[p.0][p.1])!)
-                    
-                } else {
-                    empty.points.append((p.0, p.1))
-                }
-            }
-            return empty
-        }
+        let p = (emptyRows + emptyCols + emptyGrids).min{
+            if $0.count == 0 { return false }
+            else if $1.count == 0 { return true }
+            else  { return $0.count < $1.count }
+        }!
         
-        let p = positions.map(findEmptyPosition(_:)).filter ({ $0.count > 0
-        }).min(by: {$0.count < $1.count})
-        
-        if var p = p {
-            
+        if p.count > 0 {
+            var valuesToFill = Array(p.valuesToFill)
+            let points = Array(p.emptyPoints)
             outer: for _ in 1...p.count.factorial {
-                
-                p.values.nextPermutations()
+                valuesToFill.nextPermutations()
                 for j in 0..<p.count {
-                    if !isValid(p.points[j].0, j: p.points[j].1, val: p.values[j]) {
+                    if !isValid(points[j].i, j: points[j].j, val: valuesToFill[j]) {
                         continue outer
                     }
                 }
-                
+                // fill value
                 for j in 0..<p.count {
-                    board[p.points[j].0][p.points[j].1] = p.values[j]
+                    board[points[j].i][points[j].j] = valuesToFill[j]
+                    emptyRows[points[j].i].fill(point: points[j], value: valuesToFill[j])
+                    emptyCols[points[j].j].fill(point: points[j], value: valuesToFill[j])
+                    emptyGrids[points[j].i/3*3+points[j].j/3].fill(point: points[j], value: valuesToFill[j])
                 }
-                
                 if solveSudoku(&board) { return true }
-                
+                // empty value
                 for j in 0..<p.count {
-                    board[p.points[j].0][p.points[j].1] = "."
+                    board[points[j].i][points[j].j] = "."
+                    emptyRows[points[j].i].empty(point: points[j], originalValue: valuesToFill[j])
+                    emptyCols[points[j].j].empty(point: points[j], originalValue: valuesToFill[j])
+                    emptyGrids[points[j].i/3*3+points[j].j/3].empty(point: points[j], originalValue: valuesToFill[j])
                 }
-                
             }
             return false
-        } else { return true }
+        } else {
+            return true
+        }
         
     }
     solveSudoku(&board)
+    
 }
 
-func solveSudoku(_ board: inout [[Character]]) {
+func solveSudoku1(_ board: inout [[Character]]) {
     
     @discardableResult func solveSudoku(_ board: inout [[Character]]) -> Bool {
         
